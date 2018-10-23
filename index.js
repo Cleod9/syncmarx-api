4dfd7a90059c45cbbdaaf5dd11ef75fddfbf0457
Greg McLeod <cleod9@gmail.com>
2018-10-23 13:49:47 -0400

    -New config format to support adding more providers -Added Google Drive as a provider -Added HTML version of the token display page -Added package-lock -Added note about Edge support to website

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
+var fs = require('fs');
 var config = require('config');
 require('isomorphic-fetch');
 var express = require('express');
 var bodyParser = require('body-parser');
 var bluebird = require('bluebird');
 var cors = require('cors');
-var Dropbox = require('dropbox').Dropbox;
 
+// Providers
+var Dropbox = require('dropbox').Dropbox;
+var google = require('googleapis').google;
+
+var settings = {
+  dropbox: {
+    CLIENT_ID: config.dropbox.CLIENT_ID,
+    CLIENT_SECRET: config.dropbox.CLIENT_SECRET,
+    REDIRECT_URI: config.dropbox.REDIRECT_URI
+  },
+  googledrive: {
+    CLIENT_ID: config.googledrive.CLIENT_ID,
+    CLIENT_SECRET: config.googledrive.CLIENT_SECRET,
+    REDIRECT_URI: config.googledrive.REDIRECT_URI
+  }
+}
 var CLIENT_ID = config.CLIENT_ID;
 var CLIENT_SECRET = config.CLIENT_SECRET;
 var PORT = config.PORT;
-var DROPBOX_REDIRECT_URI = config.DROPBOX_REDIRECT_URI;
 
 /**
  * Serves the express app
@@ -21,12 +36,17 @@ var serve = function () {
   app.use(cors());
 
   // URL encoded parsing support
-  app.use(bodyParser.urlencoded());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.get('/token.htm', function (req, res) {
+    // Disallow direct access to this page
+    res.status(404).send('<pre>Cannot GET /token.htm</pre>');
+  });
 
   // Static file support
   app.use(express.static('public'));
 
-  // Auth post endpoint
+  // Auth endpoint for dropbox
   app.get('/auth/dropbox', function (req, res) {
     // Require data param to be passed
     if (!req.query.code) {
@@ -34,16 +54,69 @@ var serve = function () {
       return;
     }
 
-    var dbx = new Dropbox({ clientId: CLIENT_ID, clientSecret: CLIENT_SECRET });
-    dbx.getAccessTokenFromCode(DROPBOX_REDIRECT_URI, req.query.code)
+    var dbx = new Dropbox({ clientId: settings.dropbox.CLIENT_ID, clientSecret: settings.dropbox.CLIENT_SECRET });
+    dbx.getAccessTokenFromCode(settings.dropbox.REDIRECT_URI, req.query.code)
       .then(function (token) {
-        res.status(200).send('Copy and paste the following token into the syncmarx app/extension:<br/><br/>' + token);
+        var responseText = fs.readFileSync(__dirname + '/public/token.htm', { encoding: 'utf8' });
+        responseText = responseText.replace(/\{\{TOKEN\}\}/g, token)
+        res.status(200).send(responseText);
       })
       .catch(function(error) {
         res.status(500).send('Unknown error');
-        console.log(error, CLIENT_ID, CLIENT_SECRET);
-      });
-    
+        console.log(error);
+      });  
+  });
+
+  // Auth endpoint for Google Drive
+  app.get('/auth/googledrive', function (req, res) {
+    // Require data param to be passed
+    if (!req.query.code) {
+      res.status(500).send('Missing field');
+      return;
+    }
+
+    var oauth2Client = new google.auth.OAuth2(
+      settings.googledrive.CLIENT_ID,
+      settings.googledrive.CLIENT_SECRET,
+      settings.googledrive.REDIRECT_URI
+    );
+
+    oauth2Client.getToken(req.query.code)
+      .then(function (response) {
+        var responseText = fs.readFileSync(__dirname + '/public/token.htm', { encoding: 'utf8' });
+        responseText = responseText.replace(/\{\{TOKEN\}\}/g, response.tokens.access_token + ':' + response.tokens.refresh_token)
+        res.status(200).send(responseText);
+      })
+      .catch(function(error) {
+        res.status(500).send('Unknown error');
+        console.log(error);
+      });  
+  });
+  
+  app.post('/auth/googledrive/refreshtoken', function (req, res) {
+    // Require data param to be passed
+    if (!req.query.refresh_token) {
+      res.status(500).send('Missing field');
+      return;
+    }
+
+    var oauth2Client = new google.auth.OAuth2(
+      settings.googledrive.CLIENT_ID,
+      settings.googledrive.CLIENT_SECRET,
+      settings.googledrive.REDIRECT_URI
+    );
+    oauth2Client.setCredentials({
+      refresh_token: req.query.refresh_token
+    });
+
+    oauth2Client.getAccessToken()
+      .then(function (response) {
+        res.json({ access_token: response.token });
+      })
+      .catch(function(error) {
+        res.status(500).send('Unknown error');
+        console.log(error);
+      });  
   });
 
   app.get('/', function (req, res) {
